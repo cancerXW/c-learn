cb7416399c6635c6fd5db6da94011a270f6232a4
garychan <864426004@qq.com>
2018-06-13 11:40:52 +0800

    添加3.1实例,优化C_Primer_Plus_Review_2.12.2.c并添加注释说明

--- 2.12/C_Primer_Plus_Review_2.12.2.c
+++ 2.12/C_Primer_Plus_Review_2.12.2.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	char name[10]="王广文";
-	char addres[10]="广州市";
+	//一个中文在utf-8下占3个字节,三个字所以要9个字节
+	char name[9]="王广文";
+	char addres[9]="广州市";
 	printf("您的姓名是:%s \n地址是:%s \n",name,addres);
 	return 0;
 }
